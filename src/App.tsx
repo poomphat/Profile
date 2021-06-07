8892a5ca69f202fbb84bf02af0444f47b3349671
Poomphat <pumphat@hotmail.co.th>
2021-06-07 09:59:24 +0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./component/Navbar";
+import Homepage from "./page/Homepage";
+import Workpage from "./page/Workpage";
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Switch>
+        <Route path="/mywork" >
+          <Navbar />
+          <Workpage/>
+        </Route>
+        <Route path="/">
+          <Navbar />
+          <Homepage/>
+        </Route>
+      </Switch>
+
   );
 }
 
